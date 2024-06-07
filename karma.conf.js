e1a0e57833043d2e6ad2ab41dd6a87f7f6832876
Julio Nicolas Santarelli <santarellijulio@gmail.com>
2024-06-06 23:48:48 -0300

    Chore(portal-paciente): actualiza angular 18

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/xportal-paciente'),
+      dir: require('path').join(__dirname, './coverage/portal-paciente'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'xportal-paciente'`, () => {
+  it(`should have as title 'portal-paciente'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('xportal-paciente');
+    expect(app.title).toEqual('portal-paciente');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('xportal-paciente app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('portal-paciente app is running!');
   });
 });
