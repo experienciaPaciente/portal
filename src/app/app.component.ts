2073262518c95f0b5e1fffd7155561ee1da0b7a6
Julio Nicolas Santarelli <santarellijulio@gmail.com>
2024-06-20 14:24:37 -0300

    xp-001-header: agrega navbar + button + label

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/ui/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
 })
 export class AppComponent {
   title = 'portal-paciente';
---
+++ src/app/shared/ui/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+import { LabelComponent } from '../label/label.component';
+
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [ButtonComponent, LabelComponent],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
