2ca28c686b84c21b4a16b3436fac4af5b5b64452
JSantarelli <santarellijulio@gmail.com>
2024-10-17 05:22:57 -0300

    chore(env): agrega env a gitignore

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,4 +1,4 @@
-import { EnvironmentProviders, importProvidersFrom } from '@angular/core';
+import { EnvironmentProviders } from '@angular/core';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
