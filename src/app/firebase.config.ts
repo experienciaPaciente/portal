72800c0f94f821f88ca09ec9a9e2f4c3c5138d7a
JSantarelli <santarellijulio@gmail.com>
2026-05-25 19:35:04 -0300

    style(card): comprime card.scss por warning budget

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -6,35 +6,35 @@ import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
 import { provideStorage, getStorage } from '@angular/fire/storage'
 
 // PRODUCTION ENVIRONMENT CONFIG
-// const firebaseProviders: EnvironmentProviders[] = [
-//   provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-//   provideFirestore(() => getFirestore()),
-//   provideAuth(() => getAuth()),
-//   provideStorage(() => getStorage()),
-// ];
-
-// export { firebaseProviders };
-
-
-// DEV ENVIRONMENT CONFIG (UNCOMMENT FOR LOCAL DEVELOPMENT WITH EMULATORS)
 const firebaseProviders: EnvironmentProviders[] = [
   provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-  provideFirestore(() => {
-    const firestore = getFirestore();
-    console.log('isDevMode:', isDevMode());
-    if (isDevMode()) {
-      connectFirestoreEmulator(firestore, 'localhost', 8080);
-    }
-    return firestore;
-  }),
-  provideAuth(() => {
-    const auth = getAuth();
-    if (isDevMode()) {
-      connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
-    }
-    return auth;
-  }),
+  provideFirestore(() => getFirestore()),
+  provideAuth(() => getAuth()),
   provideStorage(() => getStorage()),
 ];
 
 export { firebaseProviders };
+
+
+// DEV ENVIRONMENT CONFIG (UNCOMMENT FOR LOCAL DEVELOPMENT WITH EMULATORS)
+// const firebaseProviders: EnvironmentProviders[] = [
+//   provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+//   provideFirestore(() => {
+//     const firestore = getFirestore();
+//     console.log('isDevMode:', isDevMode());
+//     if (isDevMode()) {
+//       connectFirestoreEmulator(firestore, 'localhost', 8080);
+//     }
+//     return firestore;
+//   }),
+//   provideAuth(() => {
+//     const auth = getAuth();
+//     if (isDevMode()) {
+//       connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
+//     }
+//     return auth;
+//   }),
+//   provideStorage(() => getStorage()),
+// ];
+
+// export { firebaseProviders };
